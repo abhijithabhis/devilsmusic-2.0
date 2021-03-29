d89e580ddbe803615dea5c2e4c7eee0f4570554d
edguru <72596185+edguru@users.noreply.github.com>
2021-03-29 17:28:57 +0530

    Update __main__.py

--- tgcalls/__main__.py
+++ tgcalls/__main__.py
@@ -9,11 +9,11 @@ async def pl(__, _):
     if _.chat.id in group_call_instances.GroupsOn:
         sira.add(_.chat.id, 'out.raw')
     else:
-        await group_call_instances.set_stream(_.chat.id, 'out.raw')
+        await group_call_instances.setsong(_.chat.id, 'out.raw')
 
 
 @client.on_message(filters.me & filters.command("stop"))
 async def pl(__, _):
-    await group_call_instances.stop(_.chat.id)
+    await group_call_instances.gooff(_.chat.id)
 
 client.run()
