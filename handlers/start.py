82566de5eab0d2abe23e8de67802d79fb4311e92
abhijithabhis <80395140+abhijithabhis@users.noreply.github.com>
2021-04-11 11:08:31 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 )
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
+        f"""<b>👋🏻 Hi dear {message.from_user.first_name}!</b>
 
 I am Niva Group Radio Bot, an open-source bot that lets you play music in your Telegram groups voice chat.
 This bot is based on su music project and hamkers vc bot. 
